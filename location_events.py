f65f591c0f21bcc114bf96516856ebabb4ca5053
20074694TAFE <20074694@tafe.wa.edu.au>
2022-05-15 22:17:03 +0800

    more work

--- location_events.py
+++ location_events.py
@@ -1,15 +1,22 @@
-from xxlimited import Str
 from event import Event 
 
 class LocationEvents:
-    # String
-    #location = ""
 
-    #eventList = []
-
-    def __init__(self, location : Str):
+    def __init__(self, location : str):
         self.location = location
         self.eventList = []
+
+    def tryAddEvent(self, event : Event) -> bool:
+        if self.location != "" and event.location == self.location:
+            if len(self.eventList) != 0:
+                lastEventPresence = self.eventList[len(self.eventList)-1].presence
+                if lastEventPresence != event.presence:
+                    self.eventList.append(event)
+                    return True
+            else:
+                self.eventList.append(event)
+                return True
+        return False
 
 
 if __name__ == '__main__':
@@ -20,6 +27,11 @@ if __name__ == '__main__':
     event2.presence = True
     print(event1)
     print(event2)
-    locationEvent = LocationEvents("Kitchen",[event1,event2])
+    locationEvent = LocationEvents("Home")
     print(locationEvent.location)
-    print(locationEvent.eventList[1])
+    print(len(locationEvent.eventList))
+    locationEvent.tryAddEvent(event1)
+    locationEvent.tryAddEvent(event2)
+    print(len(locationEvent.eventList))
+    print(locationEvent.eventList[0].presence)
+    print(locationEvent.eventList[1].presence)
